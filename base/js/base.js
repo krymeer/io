4ac2a205b0a753b2a403dbdfb9d2cb6d4094619f
krymeer <krymeer@gmail.com>
2016-11-20 10:56:59 +0100

    as if nothing really matters

---
+++ base/js/base.js
@@ -0,0 +1,73 @@
+var checkWhichPage = function() {
+  var addr = window.location.href;
+  addr = addr.substring(addr.lastIndexOf('/')+1, addr.length);
+  if (addr.length != 0) {
+    $('#horizontalBar li a').each(function() {
+      var a = $(this)[0].attributes[0].nodeValue;
+      if (a == addr) {
+        $(this).append('<div class="chosen"></div>');
+      } else {
+        $(this).children().remove();
+      }
+    });
+  }
+};
+
+var addLineBreaks = function() {
+  $('#horizontalBar li a').each(function() {
+    var text = $(this)[0].outerText;
+    if (text.length >= 25) {
+      var words = text.split(' '), formattedText = '';
+      for (var i = 0; i < words.length; i++) {
+        formattedText += words[i] + ' ';
+        if ((formattedText.length + words[i].length >= 25) && i != words.length-1) {
+           formattedText += '<br/>';
+        }
+      }
+      if ($(this)[0].lastElementChild != null && $(this)[0].lastChild.className === "chosen") {
+        $(this).html(formattedText + $(this)[0].lastElementChild.outerHTML);
+      } else {
+        $(this).html(formattedText);
+      }
+    }
+  });
+};
+
+var activeSearch = function() {
+  $('#horizontalBar #inputWrapper input').on('input', function() {
+    $('#horizontalBar li').css({'border-top': '1px solid #6c6a65', 'border-bottom': '0' });
+    $('#horizontalBar li:last-child').css('border-bottom', '1px solid #6c6a65');
+    var content = $(this).val().toLowerCase();
+    var i = -1;
+    $('#horizontalBar li a').each(function() {
+      var group = $(this)[0].outerText.toLowerCase();
+      if (group.indexOf(content) !== -1) {
+        $(this).css('display', 'block');
+        i++;
+      } else {
+        $(this).css('display', 'none').parent().css('border', '0');   
+      }
+    });
+    $('#horizontalBar li:visible:last').css('border-bottom', '1px solid #6c6a65');
+  });
+};
+
+$(document).ready(function() {
+  $('[data-toggle="popover"]').popover();
+  checkWhichPage();
+  addLineBreaks();
+  activeSearch();
+  $(document).on('click', function(e) {
+    $('[data-toggle="popover"]').each(function() {
+      if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
+        $(this).popover('hide').data('bs.popover').inState.click = false;
+      }
+    });
+  });
+
+  var numberOfNotifications = Math.floor((Math.random()*100));
+  /* Liczba powiadomień dla danego użytkownika; na razie to jakaś losowa wartość. */
+  if (numberOfNotifications > 0) {
+    $('#alertSquare').css('display', 'inline').html(numberOfNotifications);
+  }
+});
