5f46e2beb5880518f296534246feee0752527eeb
krymeer <krymeer@gmail.com>
2016-11-20 13:30:03 +0100

    mobile

--- base/js/base.js
+++ base/js/base.js
@@ -2,7 +2,7 @@ var checkWhichPage = function() {
   var addr = window.location.href;
   addr = addr.substring(addr.lastIndexOf('/')+1, addr.length);
   if (addr.length != 0) {
-    $('#horizontalBar li a').each(function() {
+    $('#verticalBar li a').each(function() {
       var a = $(this)[0].attributes[0].nodeValue;
       if (a == addr) {
         $(this).append('<div class="chosen"></div>');
@@ -14,7 +14,7 @@ var checkWhichPage = function() {
 };
 
 var addLineBreaks = function() {
-  $('#horizontalBar li a').each(function() {
+  $('#verticalBar li a').each(function() {
     var text = $(this)[0].outerText;
     if (text.length >= 25) {
       var words = text.split(' '), formattedText = '';
@@ -34,12 +34,12 @@ var addLineBreaks = function() {
 };
 
 var activeSearch = function() {
-  $('#horizontalBar #inputWrapper input').on('input', function() {
-    $('#horizontalBar li').css({'border-top': '1px solid #6c6a65', 'border-bottom': '0' });
-    $('#horizontalBar li:last-child').css('border-bottom', '1px solid #6c6a65');
+  $('#verticalBar #inputWrapper input').on('input', function() {
+    $('#verticalBar li').css({'border-top': '1px solid #6c6a65', 'border-bottom': '0' });
+    $('#verticalBar li:last-child').css('border-bottom', '1px solid #6c6a65');
     var content = $(this).val().toLowerCase();
     var i = -1;
-    $('#horizontalBar li a').each(function() {
+    $('#verticalBar li a').each(function() {
       var group = $(this)[0].outerText.toLowerCase();
       if (group.indexOf(content) !== -1) {
         $(this).css('display', 'block');
@@ -48,7 +48,7 @@ var activeSearch = function() {
         $(this).css('display', 'none').parent().css('border', '0');   
       }
     });
-    $('#horizontalBar li:visible:last').css('border-bottom', '1px solid #6c6a65');
+    $('#verticalBar li:visible:last').css('border-bottom', '1px solid #6c6a65');
   });
 };
 
@@ -57,7 +57,14 @@ $(document).ready(function() {
   checkWhichPage();
   addLineBreaks();
   activeSearch();
-  $(document).on('click', function(e) {
+  $('#moreButton').click(function() {
+    if (!$('#verticalBar').is(':visible')) {
+      $('#verticalBar').css('display', 'inline-block');
+    } else {
+      $('#verticalBar').css('display', 'none');
+    }
+  });
+  $(document).click(function(e) {
     $('[data-toggle="popover"]').each(function() {
       if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
         $(this).popover('hide').data('bs.popover').inState.click = false;
