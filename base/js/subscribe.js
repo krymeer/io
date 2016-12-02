39750298952ebd89952b8255720794a273d8d616
krymeer <krymeer@gmail.com>
2016-12-02 18:54:30 +0100

    list

--- base/js/subscribe.js
+++ base/js/subscribe.js
@@ -15,43 +15,30 @@ var setProperHeight = function() {
 	}
 };
 
+var enableClicks = function() {
+	$('.titleOfCourse').click(function() {
+		$('.titleOfCourse span').remove();
+		if ($(this).next().css('display') == 'table') {
+			$(this).next().hide();
+		} else {
+			$('.courseDetails').hide();
+			$(this).children('tbody').children('tr').children('td:last').append('<span class="chosen"></span>');
+			$(this).next().show();
+		}
+	});
+	$('.courseDetails i').click(function() {
+		var tr = $(this).closest('tr');
+		if (tr.hasClass('chosenGroup')) {
+			tr.removeClass('chosenGroup');
+			tr.children('td').children('i').html('check_box_outline_blank');
+		} else {
+			tr.addClass('chosenGroup');
+			tr.children('td').children('i').html('check_box');
+		}
+	});
+};
+
 $(window).on('load', function() {
 	setProperHeight();
+	enableClicks();
 });
-/* OBSŁUGA ZAPYTAŃ GET
-	$('#selectDep').hide();
-	$('#selectFld').hide();
-	$('#selectUni select').change(function() { 
-		var university = ($('#selectUni select option:selected').attr('id'));
-		$.get('https://whatever.test/example.php', {uni: university})
-			.done(function(data) {
-				var faculties = JSON.parse(data);
-				$('#selectDep').html('<span class="title">Wydział</span><select class="selectpicker" data-width="300px" data-style="btn-primary" title=" "></select>');
-				for (var i = 0; i < faculties.length; i++) {
-					var option = '<option id="' + faculties[i][1] + '">' + faculties[i][0] + '</option>'
-					$('#selectDep .selectpicker').append(option);
-				}
-				$('.selectpicker').selectpicker('refresh');
-				setProperHeight();
-				$('#selectDep').fadeIn('fast');
-				$('#selectDep select').change(function() { 
-					var faculty = ($('#selectDep select option:selected').attr('id'));
-					$.get('https://whatever.test/example.php', {fac: faculty})
-					.done(function(data) {
-						var faculties = JSON.parse(data);
-						$('#selectFld').html('<span class="title">Kierunek</span><select class="selectpicker" data-width="300px" data-style="btn-primary" title=" "></select>');
-						for (var i = 0; i < faculties.length; i++) {
-							var option = '<option id="' + faculties[i][1] + '">' + faculties[i][0] + '</option>'
-							$('#selectFld .selectpicker').append(option);
-						}
-						$('.selectpicker').selectpicker('refresh');
-						setProperHeight();
-						$('#selectFld').fadeIn('fast');
-					});
-				});
-			})
-			.fail(function() {
-				console.error('Problem with getting a list of faculties!')
-			})
-	});
-*/
