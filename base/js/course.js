9acb4689e50b5076a9d90c79970a5273f0b2f72f
krymeer <krymeer@gmail.com>
2016-12-11 22:40:26 +0100

    tests

--- base/js/course.js
+++ base/js/course.js
@@ -1,19 +1,18 @@
 $(document).ready(function() {
-  var content3 = $('.coursePanel:nth-of-type(3)'),
-      content4 = $('.coursePanel:nth-of-type(4)'),
-      flag = 0;
-  if ($('i.mobileHeader').css('display') !== 'none') {
+  var flag = 0;
+  if ($('a.mobileHeader').css('display') !== 'none') {
+    var content3 = $('.coursePanel:nth-of-type(3)'),
+        content4 = $('.coursePanel:nth-of-type(4)');
     $(content3).replaceWith(content4);
     $('.coursePanel:nth-of-type(3)').after(content3);
-    flag = 1;
   }
   $(window).resize(function() {
-    alert('coś')
     if (window.innerWidth >= 720) {
       $(content4).replaceWith(content3);
       $('.coursePanel:nth-of-type(3)').after(content4);
       $('.panelContent').css('display', 'block');
-    };
+      flag = 1;
+    }
   })
   $('.reportError').click(function() {
     alert($(this).parent().children('h4').html())
@@ -23,7 +22,7 @@ $(document).ready(function() {
   }, function() {
     $(this).popover('hide');
   });
-  $('i.mobileHeader').click(function() {
+  $('a.mobileHeader').click(function() {
     var content = $(this).parent().next('.panelContent');
     if (content.css('display') === 'none') {
       $('.panelContent').css('display', 'none');
