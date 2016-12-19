3199f37ab4945eeeb96d57c4ebc8b020bafb4ae9
krymeer <krymeer@gmail.com>
2016-12-19 01:05:03 +0100

    Popups

--- form/js/errors.js
+++ form/js/errors.js
@@ -1,29 +1,43 @@
 $(document).ready(function() {
-  var numberOfErrors = $('[id^=errorPanel]').length;
-  if (numberOfErrors > 0) {
+  var positioning = function() {
     if ($(window).width() > 720) {
-      $('[id^=errorPanel]').css({
-        'top': $('#form').position().top/2,
-        'right': '50%',
-        'margin-top': -$('[id^=errorPanel]').height(),
-        'margin-right': -$('[id^=errorPanel]').width()/2,
+      $('.popup').each(function() {
+        $(this).css({
+          'top': $('#form').position().top/2,
+          'right': '50%',
+          'margin-top': -$('.popup').height(),
+          'margin-right': -$(this).width()/2
+        });
+      })
+    } else {
+      $('.popup').each(function() {
+        $(this).css({
+          'top': '0',
+          'right': '0',
+          'margin-top': '0',
+          'margin-right': '0'
+        })
       });
     }
+    var numberOfErrors = $('.popup').length;
     if (numberOfErrors > 1) {
-      var w = 0;
+      var w = 1;
       for (var i = 1; i < numberOfErrors; i++) {
-        if ($(window).width() > 720) {
-          w = 1;
-        } else {
-          w = 2;
+        if ($(window).width() <= 720 && i == 1) {
+          w += 1;
         }
-        $('#errorPanel' + i).css('margin-top', $('[id^=errorPanel]').height()*w);
+        $('.popup#'+i).css('margin-top', $('.popup').height()*w);
+        w += 2;
       }
     }
-    $('[id^=errorPanel] i').click(function() {
-      var name = $(this).parent()[0].id;
-      var n = name.substring(10, name.length);
-      $('#errorPanel' + n).fadeOut('fast');
-    });
   }
+  positioning();
+  $('.popup i').click(function() {
+    var n = $(this).parent().attr('id');
+    $('.popup#' + n).fadeOut('fast');
+    console.log(n)
+  });
+  $(window).resize(function() {
+    positioning();
+  })
 });
