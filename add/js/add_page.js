208cb740da5be7eef8472af493d9c6c599dc52c3
krymeer <krymeer@gmail.com>
2017-01-01 11:56:13 +0100

    new calendar

--- add/js/add_page.js
+++ add/js/add_page.js
@@ -4,23 +4,57 @@ var addCaret = function() {
   });
 }
 
-/*
-webshim.setOptions('forms-ext', {
-  replaceUI: 'auto',
-  types: 'date',
-  date: {
-    startView: 2,
-    inlinePicker: true,
-    classes: 'hide-inputbtns'
-  }
-});
-webshim.activeLang('pl');
-webshim.polyfill('forms forms-ext');
-*/
-
-
 $(document).ready(function() {
-  $('.info_calendar').hide();
   $('select').niceSelect();
   addCaret();
+  $('#datepicker').datepicker({
+    'weekStart': 1, 
+    'language': 'pl',
+    'startDate': '-1y',
+    'endDate': '+2y',
+    'format': 'd MM yyyy'
+  });
+  $('#datepicker').on('changeDate', function() {
+    var date = $('#datepicker').datepicker('getFormattedDate'),
+        month = date.split(' ')[1];
+    switch(month) {
+      case 'Styczeń':
+        date = date.replace(month, 'stycznia');
+        break;
+      case 'Luty':
+        date = date.replace(month, 'lutego');
+        break;
+      case 'Marzec':
+        date = date.replace(month, 'marca');
+        break;
+      case 'Kwiecień':
+        date = date.replace(month, 'kwietnia');
+        break;
+      case 'Maj':
+        date = date.replace(month, 'maja');
+        break;
+      case 'Czerwiec':
+        date = date.replace(month, 'czercwa');
+        break;
+      case 'Lipiec':
+        date = date.replace(month, 'lipca');
+        break;
+      case 'Sierpień':
+        date = date.replace(month, 'sierpnia');
+        break;
+      case 'Wrzesień':
+        date = date.replace(month, 'września');
+        break;
+      case 'Październik':
+        date = date.replace(month, 'października');
+        break;
+      case 'Listopad':
+        date = date.replace(month, 'listopada');
+        break;
+      case 'Grudzień':
+        date = date.replace(month, 'grudnia');
+        break;
+    }
+    $('#hiddenInput').val(date);
+  });
 });
