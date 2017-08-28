f7fbf76a1a080ccaa4ba25063145d5aadbb3bc50
krymeer <krymeer@gmail.com>
2017-08-28 19:20:10 +0200

    Basics of creating files TBC

--- file-creator/main.js
+++ file-creator/main.js
@@ -9,20 +9,45 @@ function create_file(data, filename) {
   document.body.removeChild(link);
 }
 
-$(document).ready(function() {
-  $('#create_file').click(function() {
-    var data = $('textarea#file_contents').val(),
-        filename = $('input#filename').val();
-
-    if (filename === '') {
-      $('#filename ~ .alert_msg')
+function is_empty(field_value, object) {
+  var alert_block = $('#' + object.attr('id') + ' ~ .alert_msg'),
+      width = object.outerWidth();
+  if (field_value === '') {
+    if (alert_block.css('display') === 'none') {
+      alert_block
         .html('This field cannot be empty')
-        .css('width', $('input#filename').outerWidth())
+        .css('width', width)
         .slideDown('fast');
-    } else {
-      if ($('#filename ~ .alert_msg').css('display') !== 'none') {
-        $('#filename ~ .alert_msg').slideUp('fast')
+    }
+    return true;
+  }
+  if (alert_block.css('display') !== 'none') {
+    alert_block.slideUp('fast');
+  }
+  return false;
+};
+
+$(document).ready(function() {
+  $('#clear_data').click(function() {
+    $('.alert_msg').each(function() {
+      if ($(this).css('display') !== 'none') {
+        $(this).slideUp('fast');
       }
+    });
+    $('textarea, input').each(function() {
+      $(this).val('');
+    });
+  });
+  $('#create_file').click(function() {
+    var textarea = $('textarea#file_contents'),
+        data = textarea.val(),
+        input = $('input#filename'),
+        filename = input.val();
+
+    var cond1 = is_empty(data, textarea),
+        cond2 = is_empty(filename, input);
+
+    if (!cond1 && !cond2) {
       create_file(data, filename + '.html');
     }
   }); 
