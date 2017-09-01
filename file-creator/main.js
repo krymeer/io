ac2e700d62f62147a1d00e1e6b53d41b3251fca4
krymeer <krymeer@gmail.com>
2017-09-01 22:38:44 +0200

    Moving validate_filename() function into a separate file

--- file-creator/main.js
+++ file-creator/main.js
@@ -1,10 +1,3 @@
-function validate_filename(e) {
-  return ((e.charCode === 95) 
-    || (e.charCode >= 48 && e.charCode <= 57)
-    || (e.charCode >= 65 && e.charCode <= 90)
-    || (e.charCode >= 97 && e.charCode <= 122));
-}
-
 function create_file(data, filename) {
   var encoded_uri = encodeURI(data),
       link = document.createElement('a');
---
+++ js/validate_filename.js
@@ -0,0 +1,6 @@
+function validate_filename(e) {
+  return ((e.charCode === 95) 
+    || (e.charCode >= 48 && e.charCode <= 57)
+    || (e.charCode >= 65 && e.charCode <= 90)
+    || (e.charCode >= 97 && e.charCode <= 122));
+}
