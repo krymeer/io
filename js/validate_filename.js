a8d2bf394449373c0b00bba206c9a4801e0e46d8
krymeer <krymeer@gmail.com>
2017-09-03 11:12:07 +0200

    Added a digits detecting function

--- js/validate_filename.js
+++ js/validate_filename.js
@@ -4,3 +4,7 @@ function validate_filename(e) {
     || (e.charCode >= 65 && e.charCode <= 90)
     || (e.charCode >= 97 && e.charCode <= 122));
 }
+
+function check_if_digit(e) {
+  return (e.charCode >= 48 && e.charCode <= 57);
+}
