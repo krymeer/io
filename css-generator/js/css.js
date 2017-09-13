e6f0b2d0a1bb3fda8ee209a40d5b87f3c65ff880
krymeer <krymeer@gmail.com>
2017-09-13 18:09:48 +0200

    CSS output: proper styling

--- css-generator/js/css.js
+++ css-generator/js/css.js
@@ -1,6 +1,6 @@
-var basic_css = '\n/* @import lines */\n\
+var basic_css = '\n\
 @import url("https://fonts.googleapis.com/css?family=Saira+Semi+Condensed");\n\
-/* @import_end */\n\
+\n\
 html, body { \n\
   padding: 0; \n\
   margin: 0; \n\
@@ -16,15 +16,12 @@ html, body { \n\
   width: 90%; \n\
   height: 840px; \n\
   margin: 10px auto; \n\
+  grid-template: 1fr 1fr 1fr / auto; \n\
 } \n\
 \
 #main_container, header, #main_content, footer { \n\
   display: grid; \n\
 } \n\
-\
-#main_container { \n\
-  grid-template: 1fr 1fr 1fr / auto; \n\
-}\n\
 \
 div.item { \n\
   height: 100%; \n\
@@ -38,20 +35,19 @@ var no_lato = true, no_roboto = true, tab = '  ';
 function move_css(grid, name) {
   var style = grid.attr('style');
   if (style !== undefined) {
-    basic_css += '\n' + name + ' {\n  ' + grid.attr('style') + '\n}';
+    basic_css += '\n' + name + tab + '{\n' + tab + grid.attr('style') + '\n}';
     grid.removeAttr('style');
   }
   $('.item', grid).each(function() {
     style = $(this).attr('style');
     style = style.replace(/; /g, ';\n  ');
-    basic_css += '\n#' + $(this).attr('id') + ' {\n  ' + style + '\n}';
+    basic_css += '\n#' + $(this).attr('id') + tab + '{\n' + tab + style + '\n}';
     if (style.indexOf('Roboto') >= 0 && no_roboto) {
-      basic_css += '\n@import url("https://fonts.googleapis.com/css?family=Roboto");'
+      basic_css = '\n@import url("https://fonts.googleapis.com/css?family=Roboto");' + basic_css;
     }
     if (style.indexOf('Lato') >= 0 && no_lato) {
-      basic_css += '\n@import url("https://fonts.googleapis.com/css?family=Lato");'
+      basic_css = '\n@import url("https://fonts.googleapis.com/css?family=Lato");' + basic_css;
     }
-    //basic_css = basic_css.replace(/\n$/, '')
     $(this).removeAttr('style');
   });
   return grid;
