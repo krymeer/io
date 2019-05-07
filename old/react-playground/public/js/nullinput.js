4bac3b786e4a64f7b1d51221b869bbd21df8c254
krymeer <krzysztof.radoslaw.osada@gmail.com>
2019-05-07 21:13:06 +0200

    Removing old unnecessary files

--- old/react-playground/public/js/nullinput.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-    var node = document.getElementById('root');
-
-    ReactDOM.render(React.createElement("input", { value: "hi" }), node);
-
-    setTimeout(function () {
-        ReactDOM.render(React.createElement("input", { value: null }), node);
-    }, 5000);
-};
